9c2ab3c4493832387760347e7c92377d08127ade
KyawZinAung12 <ukza11977296@gmail.com>
2018-06-06 04:24:44 -0500

    Test

---
+++ ex2.py
@@ -0,0 +1,4 @@
+# a command, this is so you can read your program later.
+print ("I would have code like this.") #and the comment after is ignored.
+#print "This is not run"
+print ("This will run.")
