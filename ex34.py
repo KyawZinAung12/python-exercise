73d80f219f9450f4626b0120e1b25eec471dd348
KyawZinAung12 <ukza11977296@gmail.com>
2018-06-15 02:03:32 -0500

    Test

---
+++ ex34.py
@@ -0,0 +1,14 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+python =animals[1]
+peacock =animals[2]
+kangaroo =animals[3]
+whale =animals[4]
+platypus =animals[5]
+
+print(f"The first animals is at 0 and is a {bear}")
+print(f"The third animals is at 2 and is a {peacock}")
+print(f"The first animals is {bear}")
+print(f"the animals at 3 is {peacock}")
+print(f"the fifth animals is {whale}")
+print(f"the sixth animals is {platypus}")
